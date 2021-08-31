d3c16310b504310c8582b364065af4727dc710dc
elliothwang <elliothwang@gmail.com>
2021-08-31 13:38:11 -0700

    style majorStats component

--- src/components/USMap/USMap.jsx
+++ src/components/USMap/USMap.jsx
@@ -1,8 +1,12 @@
 import './USMap.css';
-import React  from 'react';
+import React from 'react';
+import { defineGrid } from 'honeycomb-grid';
 
 export default function USMap() {
+  const Grid = defineGrid();
+  Grid.rectangle({ width: 4, height: 4 });
+  
   return (
-    <div className="mapContainer">US MAP</div>
+    <div className="mapContainer">us map</div>
   )
 }
