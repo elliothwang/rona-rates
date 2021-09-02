1d3495f99199dae974bae17aaa9cd038b9da7634
elliothwang <elliothwang@gmail.com>
2021-09-01 18:28:32 -0700

    style sideNavBar

--- src/components/MajorStats/MajorStats.jsx
+++ src/components/MajorStats/MajorStats.jsx
@@ -1,9 +1,16 @@
 import './MajorStats.css';
 import React, { useEffect, useState }  from 'react';
+import moment from 'moment';
 const axios = require('axios').default;
 
 export default function MajorStats() {
+  let yesterday = moment().subtract(1, 'days').format('l'); 
   const [USData, setUSData] = useState("");
+
+  function numberWithCommas(x) {
+    if (x) return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    else return null;
+  }
 
   function getUSData() {
     axios.get('https://corona.lmao.ninja/v2/countries/USA?yesterday=true&strict=true&query')
@@ -18,27 +25,31 @@ export default function MajorStats() {
 
   useEffect(() => getUSData(), []);
 
-  function numberWithCommas(x) {
-    if (x) return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    else return null;
-  }
-
   return (
       <div className="info">
         <div className="cases">
           <div className="title">Cases</div>
           <div className="totalCases">{numberWithCommas(USData.cases)}</div>
-          <div className="newCases">+{numberWithCommas(USData.todayCases)}</div>
+          <div className="newCases flex-ctr-ctr">
+            <div>+{numberWithCommas(USData.todayCases)}</div>
+            <div className="newDate">&nbsp; ({yesterday})</div>
+          </div>
         </div>
         <div className="deaths">
           <div className="title">Deaths</div>
           <div className="totalDeaths">{numberWithCommas(USData.deaths)}</div>
-          <div className="newDeaths">+{numberWithCommas(USData.todayDeaths)}</div>
+          <div className="newDeaths flex-ctr-ctr">
+            <div>+{numberWithCommas(USData.todayDeaths)}</div>
+            <div className="newDate">&nbsp; ({yesterday})</div>
+          </div>
         </div>
         <div className="recoveries">
           <div className="title">Recoveries</div>
           <div className="totalRecoveries">{numberWithCommas(USData.recovered)}</div>
-          <div className="newRecoveries">+{numberWithCommas(USData.todayRecovered)}</div>
+          <div className="newRecoveries flex-ctr-ctr">
+            <div>+{numberWithCommas(USData.todayRecovered)}</div>
+            <div className="newDate">&nbsp; ({yesterday})</div>
+          </div>
         </div>
       </div>
   )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,13 +14,13 @@ export default function NavBar({ user, setUser }) {
   function openOrCloseSideNav() {
     if (!sideNavOpen) {
       document.querySelector(".sideNavBar").classList.add('sideNavOpened');
-      document.querySelector(".openSideNavButton").classList.add('hideSideNav');
-      document.querySelector(".closeSideNavButton").classList.remove('hideSideNav');
+      document.querySelector(".openSideNavButton").classList.remove('hide');
+      document.querySelector(".closeSideNavButton").classList.add('hide');
       setSideNavOpen(true);
     } else {
       document.querySelector(".sideNavBar").classList.remove('sideNavOpened');
-      document.querySelector(".openSideNavButton").classList.remove('hide');
-      document.querySelector(".closeSideNavButton").classList.add('hide');
+      document.querySelector(".openSideNavButton").classList.add('hide');
+      document.querySelector(".closeSideNavButton").classList.remove('hide');
       setSideNavOpen(false);
     };
   };
@@ -33,23 +33,25 @@ export default function NavBar({ user, setUser }) {
         <NavLink to="" className="logOutTopNav" onClick={handleLogOut}>Log Out</NavLink>
         <span className="welcomeTopNav">Welcome, {user.name}</span>
       </div>
-      <div className="sideNavButton" onClick={openOrCloseSideNav}>
-          <div className="openSideNavButton">
+      <div className="sideNavButtonContainer">
+        <div className="sideNavButton" onClick={openOrCloseSideNav}>
+          <div className="openSideNavButton hide">
             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
             </svg>
           </div>
-          <div className="closeSideNavButton hide">
+          <div className="closeSideNavButton">
             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"/>
             </svg>
           </div>
         </div>
+      </div>
       <div className="sideNavBar">
         <NavLink to="/" exact className="homeSideNav" name="activeHome" activeStyle={{color : "white"}} onClick={openOrCloseSideNav}>Home</NavLink>
         <NavLink to="/stats"className="statsSideNav" name="activeStats" activeStyle={{color : "white"}} onClick={openOrCloseSideNav}>Stats</NavLink>
-        <NavLink to="" className="logOutSideNav" onClick={handleLogOut}>Log Out</NavLink>
         <span className="welcomeSideNav">Welcome, {user.name}</span>
+        <NavLink to="" className="logOutSideNav" onClick={handleLogOut}>Log Out</NavLink>
       </div>
     </nav>
   );
