cfcd38f245d67bf1da855512675efb893187fb84
elliothwang <elliothwang@gmail.com>
2021-10-07 21:19:41 -0700

    temporarily remove stateStats component

--- src/pages/StatsPage/StatsPage.jsx
+++ src/pages/StatsPage/StatsPage.jsx
@@ -4,14 +4,14 @@ import StateImg from '../../components/StateImg/StateImg'
 import StateStats from '../../components/StateStats/StateStats'
 
 export default function StatsPage({ apiData }) {
-  const stateName = localStorage.getItem('state');
+  const stateName = localStorage.getItem('storageStateName');
   const state = apiData.find((state) => state.state === stateName);
 
   function storeStateData() {
-    localStorage.setItem('stateData', JSON.stringify(state));
+    localStorage.setItem('storageStateData', JSON.stringify(state));
   };
 
-  useEffect(() => storeStateData(), [stateName]);
+  useEffect(() => storeStateData());
 
   return (
   <div>
