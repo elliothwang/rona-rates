6c4d0b1f865f86167908bbf9201359870a381766
elliothwang <elliothwang@gmail.com>
2021-10-09 21:09:36 -0700

    home page dynamic styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import React, { Fragment, useState }  from 'react';
 
 export default function Footer() {
   return (
