1277a1fd0aac448ae5c528ee9b23708bbe33deec
elliothwang <elliothwang@gmail.com>
2021-10-12 18:33:17 -0700

    update pcakage.json

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`);
