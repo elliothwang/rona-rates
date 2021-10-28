3e6ffa2b58524b5ba3675f6756c79966eee3ed8e
elliothwang <elliothwang@gmail.com>
2021-10-27 21:49:24 -0700

    responsive styling dashboard page

--- src/components/CountyStatsContainer/CountyStatsContainer.jsx
+++ src/components/CountyStatsContainer/CountyStatsContainer.jsx
@@ -4,7 +4,7 @@ import * as help from '../../utilities/helper-functions';
 
 export default function CountyStatsContainer({ title, stat1, idx, casesShown }) {
   return (
-    <div className="countyStatsOuterContainer flex-ctr-ctr">
+    <div className="countyStatsContainer flex-ctr-ctr">
       <div className={idx < 5 ? "countyIdx red flex-ctr-ctr" : "countyIdx flex-ctr-ctr"}>{idx + 1}.</div>
       <div className="countyName flex-ctr-ctr">{title}</div>
       <div className={ casesShown ? "countyCasesStat flex-ctr-ctr" : "countyDeathsStat red flex-ctr-ctr"}>{help.addCommas(stat1)}</div>
