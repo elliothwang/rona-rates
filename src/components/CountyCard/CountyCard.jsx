2c76293b7bc83d121de3e60afd6d55b694d7a9c2
elliothwang <elliothwang@gmail.com>
2021-11-07 19:11:59 -0800

    mobile styling

--- src/components/CountyCard/CountyCard.jsx
+++ src/components/CountyCard/CountyCard.jsx
@@ -5,9 +5,9 @@ import * as help from '../../utilities/helper-functions';
 export default function CountyCard({ title, stat1, idx, casesShown }) {
   return (
     <div className="countyCard flex-ctr-ctr">
-      <div className={idx < 3 ? "countyIdx red flex-ctr-ctr" : "countyIdx flex-ctr-ctr"}>{idx + 1}.</div>
-      <div className="countyName flex-ctr-ctr">{title}</div>
-      <div className={ casesShown ? "countyCasesStat flex-ctr-ctr" : "countyDeathsStat red flex-ctr-ctr"}>{help.addCommas(stat1)}</div>
+      <div className="countyIdx">{idx + 1}.</div>
+      <div className="countyName">{title}</div>
+      <div className={ casesShown ? "countyCasesStat" : "countyDeathsStat red flex-ctr-ctr"}>{help.addCommas(stat1)}</div>
     </div>
   )
 }
