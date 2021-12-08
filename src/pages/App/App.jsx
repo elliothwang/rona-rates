60f321332b4408a913d3004dbd71493836dc5b9e
elliothwang <elliothwang@gmail.com>
2021-12-07 17:26:55 -0800

    test route

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -15,13 +15,13 @@ export default function App() {
       <Fragment>
         <NavBar user={user} setUser={setUser} />
         <Switch>
-          <Route path="/corona-rona/:state">
+          <Route path="/home/:state">
             <StatePage user={user} />
           </Route>
-          <Route path="/corona-rona">
+          <Route path="/home">
             <DashboardPage user={user} />
           </Route>
-          <Redirect to="/corona-rona" />
+          <Redirect to="/home" />
         </Switch>
         <Footer />
       </Fragment>
