43a8d6250c59040c40b189d9b4cedb0e16c0baa4
elliothwang <elliothwang@gmail.com>
2021-12-13 16:52:18 -0800

    fix typo

--- src/utilities/refreshToken.js
+++ src/utilities/refreshToken.js
@@ -3,7 +3,7 @@ export const refreshTokenSetup = (res) => {
 
   const refreshToken = async () => {
     const newAuthRes = await res.reloadAuthResponse();
-    resfreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
     setTimeout(refreshToken, refreshTiming);
   };
   setTimeout(refreshToken, refreshTiming);
