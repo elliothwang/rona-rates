d789e98f7da28fc8fcf3683b8626674286eb88bb
elliothwang <elliothwang@gmail.com>
2023-02-10 13:27:12 -0800

    add additional ! FIX comment

--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -20,7 +20,7 @@ export default function StatCard({
 }) {
   const [isFlipped, setIsFlipped] = useState(false);
 
-  // ! FIX stat3 is returning '0' as the api's 'today' stats are returning '0'. try different api?
+  // ! FIX: stat3 is returning '0' as the api's 'today' stats are returning '0'. try different api?
 
   function handleCardFlip(evt) {
     evt.preventDefault();
--- src/index.js
+++ src/index.js
@@ -18,6 +18,16 @@ root.render(
   document.getElementById('root')
 );
 
+// ! FIX: Could not resolve dependency: peer react@"^16.13.1"
+//        from react-usa-map@1.5.0 node_modules/react-usa-ma
+//        react-usa-map@"^1.5.0" from the root project
+// ! FIX: Conflicting peer dependency: react@16.14.0 node_modules/react
+//        peer react@"^16.13.1" from react-usa-map@1.5.0 node_modules/react-usa-map
+//        react-usa-map@"^1.5.0" from the root project
+
+// Fix the upstream dependency conflict, or retry this command with --force, or --legacy-peer-deps
+// to accept an incorrect (and potentially broken) dependency resolution.
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
