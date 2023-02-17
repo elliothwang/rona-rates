c2374b7688503936ec7ba6a4e062b5b0b4234296
elliothwang <elliothwang@gmail.com>
2023-02-17 10:34:45 -0800

    force packages

--- src/index.js
+++ src/index.js
@@ -19,13 +19,6 @@ root.render(
   document.getElementById('root')
 );
 
-// ! FIX: Could not resolve dependency: peer react@"^16.13.1"
-//        from react-usa-map@1.5.0 node_modules/react-usa-ma
-//        react-usa-map@"^1.5.0" from the root project
-// ! FIX: Conflicting peer dependency: react@16.14.0 node_modules/react
-//        peer react@"^16.13.1" from react-usa-map@1.5.0 node_modules/react-usa-map
-//        react-usa-map@"^1.5.0" from the root project
-
 // Fix the upstream dependency conflict, or retry this command with --force, or --legacy-peer-deps
 // to accept an incorrect (and potentially broken) dependency resolution.
 
